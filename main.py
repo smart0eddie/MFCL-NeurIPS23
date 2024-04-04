638723f830eb8ab8b3e668459bc150b4d8e6d254
smart0eddie <smart0eddie@gmail.com>
2024-04-05 02:31:56 +0800

    wrap the main.py to pevent multi thread crash in Window

--- main.py
+++ main.py
@@ -16,91 +16,95 @@ from clients.simple import AVG, PROX, ORACLE
 from data_prep.super_imagenet import SuperImageNet
 from utiles import setup_seed, fedavg_aggregation, evaluate_accuracy_forgetting, evaluate_accuracy, train_gen, start
 
-# wandb.init()
+def main():
+    # wandb.init()
 
-args = start()
-if torch.cuda.is_available():
-    args.device = "cuda"
-    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuID
-else:
-    args.device = "cpu"
+    args = start()
+    if torch.cuda.is_available():
+        args.device = "cuda"
+        os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuID
+    else:
+        args.device = "cpu"
 
-setup_seed(args.seed)
+    setup_seed(args.seed)
 
-if args.dataset == constant.CIFAR100:
-    dataset = CL_dataset(args)
-    feature_extractor = ResNet18(args.num_classes, cifar=True)
-    ds = dataset.train_dataset
-elif args.dataset == constant.tinyImageNet:
-    dataset = CL_dataset(args)
-    feature_extractor = ResNet18(args.num_classes, cifar=False)
-    ds = dataset.train_dataset
-    args.generator_model = 'TINYIMNET_GEN'
-elif args.dataset == constant.SuperImageNet:
-    from models.imagenet_resnet import resnet18
-    dataset = SuperImageNet(args.path, version=args.version, num_tasks=args.n_tasks, num_clients=args.num_clients, batch_size=args.batch_size)
-    args.num_classes = dataset.num_classes
-    feature_extractor = resnet18(args.num_classes)
-    args.generator_model = 'IMNET_GEN'
-    args.img_size = dataset.img_size
-    ds = dataset
+    if args.dataset == constant.CIFAR100:
+        dataset = CL_dataset(args)
+        feature_extractor = ResNet18(args.num_classes, cifar=True)
+        ds = dataset.train_dataset
+    elif args.dataset == constant.tinyImageNet:
+        dataset = CL_dataset(args)
+        feature_extractor = ResNet18(args.num_classes, cifar=False)
+        ds = dataset.train_dataset
+        args.generator_model = 'TINYIMNET_GEN'
+    elif args.dataset == constant.SuperImageNet:
+        from models.imagenet_resnet import resnet18
+        dataset = SuperImageNet(args.path, version=args.version, num_tasks=args.n_tasks, num_clients=args.num_clients, batch_size=args.batch_size)
+        args.num_classes = dataset.num_classes
+        feature_extractor = resnet18(args.num_classes)
+        args.generator_model = 'IMNET_GEN'
+        args.img_size = dataset.img_size
+        ds = dataset
 
-global_model = network(dataset.n_classes_per_task, feature_extractor)
-teacher, generator = None, None
-gamma = np.log(args.lr_end / args.lr)
-task_size = dataset.n_classes_per_task
-counter, classes_learned = 0, task_size
-num_participants = int(args.frac * args.num_clients)
-clients, max_accuracy = [], []
-if args.method == constant.MFCL:
-    generator = models.__dict__['generator'].__dict__[args.generator_model](zdim=args.z_dim, convdim=args.conv_dim)
+    global_model = network(dataset.n_classes_per_task, feature_extractor)
+    teacher, generator = None, None
+    gamma = np.log(args.lr_end / args.lr)
+    task_size = dataset.n_classes_per_task
+    counter, classes_learned = 0, task_size
+    num_participants = int(args.frac * args.num_clients)
+    clients, max_accuracy = [], []
+    if args.method == constant.MFCL:
+        generator = models.__dict__['generator'].__dict__[args.generator_model](zdim=args.z_dim, convdim=args.conv_dim)
 
-for i in range(args.num_clients):
-    group = dataset.groups[i]
-    if args.method == constant.FedAVG:
-        client = AVG(args.batch_size, args.epochs, ds, group, args.dataset, args.device)
-    elif args.method == constant.FedProx:
-        client = PROX(args.batch_size, args.epochs, ds, group, args.dataset, args.device)
-    elif args.method == constant.ORACLE:
-        client = ORACLE(args.batch_size, args.epochs, ds, group, args.dataset, args.device)
-    elif args.method == constant.MFCL:
-        client = MFCL(args.batch_size, args.epochs, ds, group, args.client_type, args.w_kd, args.w_ft, args.syn_size, args.dataset, args.device)
-    clients.append(client)
+    for i in range(args.num_clients):
+        group = dataset.groups[i]
+        if args.method == constant.FedAVG:
+            client = AVG(args.batch_size, args.epochs, ds, group, args.dataset, args.device)
+        elif args.method == constant.FedProx:
+            client = PROX(args.batch_size, args.epochs, ds, group, args.dataset, args.device)
+        elif args.method == constant.ORACLE:
+            client = ORACLE(args.batch_size, args.epochs, ds, group, args.dataset, args.device)
+        elif args.method == constant.MFCL:
+            client = MFCL(args.batch_size, args.epochs, ds, group, args.client_type, args.w_kd, args.w_ft, args.syn_size, args.dataset, args.device)
+        clients.append(client)
 
-correct = 0 
-total = 1
-for t in range(args.n_tasks):
-    test_loader = dataset.get_full_test(t)
-    [client.set_next_t() for client in clients]
-    for round in range(args.global_round):
-        weights = []
-        lr = args.lr * np.exp(round / args.global_round * gamma)
-        selected_clients = [clients[idx] for idx in np.random.choice(args.num_clients, num_participants, replace=False)]
-        for user in selected_clients:
-            model = deepcopy(global_model)
-            user.train(model, lr, teacher, generator, counter)
-            weights.append(model.state_dict())
-        global_model.load_state_dict(fedavg_aggregation(weights))
-        if (round + 1) % args.eval_int == 0:
-            correct, total = evaluate_accuracy(global_model, test_loader, args.method, args.device)
-            print(f'round {counter}, accuracy: {100 * correct / total}')
-        counter += 1
-    if t == 0:
-        max_accuracy.append(correct / total)
-    if t > 0:
-        correct, total, accuracies = evaluate_accuracy_forgetting(global_model, dataset.get_cl_test(t), args.method)
-        print(f"total_accuracy_{t}: {accuracies}")
-        max_accuracy.append(accuracies[-1])
-    if t != args.n_tasks - 1:
-        if args.method == constant.MFCL:
-            original_global = deepcopy(global_model)
-            teacher = train_gen(deepcopy(global_model), classes_learned, generator, args)
-            for client in clients:
-                client.last_valid_dim = classes_learned
-                client.valid_dim = classes_learned + task_size
-            global_model = original_global
-        classes_learned += task_size
-        global_model.Incremental_learning(classes_learned)
+    correct = 0 
+    total = 1
+    for t in range(args.n_tasks):
+        test_loader = dataset.get_full_test(t)
+        [client.set_next_t() for client in clients]
+        for round in range(args.global_round):
+            weights = []
+            lr = args.lr * np.exp(round / args.global_round * gamma)
+            selected_clients = [clients[idx] for idx in np.random.choice(args.num_clients, num_participants, replace=False)]
+            for user in selected_clients:
+                model = deepcopy(global_model)
+                user.train(model, lr, teacher, generator, counter)
+                weights.append(model.state_dict())
+            global_model.load_state_dict(fedavg_aggregation(weights))
+            if (round + 1) % args.eval_int == 0:
+                correct, total = evaluate_accuracy(global_model, test_loader, args.method, args.device)
+                print(f'round {counter}, accuracy: {100 * correct / total}')
+            counter += 1
+        if t == 0:
+            max_accuracy.append(correct / total)
+        if t > 0:
+            correct, total, accuracies = evaluate_accuracy_forgetting(global_model, dataset.get_cl_test(t), args.method)
+            print(f"total_accuracy_{t}: {accuracies}")
+            max_accuracy.append(accuracies[-1])
+        if t != args.n_tasks - 1:
+            if args.method == constant.MFCL:
+                original_global = deepcopy(global_model)
+                teacher = train_gen(deepcopy(global_model), classes_learned, generator, args)
+                for client in clients:
+                    client.last_valid_dim = classes_learned
+                    client.valid_dim = classes_learned + task_size
+                global_model = original_global
+            classes_learned += task_size
+            global_model.Incremental_learning(classes_learned)
 
-print('forgetting:', sum([max_accuracy[i] - accuracies[i] for i in range(args.n_tasks)]) / args.n_tasks)
+    print('forgetting:', sum([max_accuracy[i] - accuracies[i] for i in range(args.n_tasks)]) / args.n_tasks)
+    
+if __name__ == "__main__":
+    main()
